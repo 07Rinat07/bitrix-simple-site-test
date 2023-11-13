d704dd14adf9e19b1d7191ed84f3b5fc1062937b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-14 01:17:33 +0500

    Создание тестовой страницы для отладки_Create test-page for debug

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001699904673';
-$dateexpire = '001699908333';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1699904731;}';
+$datecreate = '001699905264';
+$dateexpire = '001699908924';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1699905322;}';
 return true;
 ?>
