d6482db8d3b942d8db19f6f48b05135e7d7ec445
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-13 21:38:35 +0500

    Создание логической структуры сайта__Create site logics tructure

---
+++ example2/.section.php
@@ -0,0 +1,2 @@
+<?php
+$sSectionName="example2";
--- example2/index.php
+++ example2/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+$APPLICATION->SetTitle("Title example#2");
 ?>
 hello world 2
 <?php
